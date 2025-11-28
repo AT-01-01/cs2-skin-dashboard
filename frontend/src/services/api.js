47a60eb61505daffdf7b3804272983cd50018dca
AT-01-01 <1806117586@qq.com>
2025-11-28 23:43:03 +0800

    Delete frontend/src/services/api.js

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/services/api.js
-import axios from 'axios';
-const BACKEND = import.meta.env.VITE_BACKEND_ORIGIN || 'http://localhost:8080';
-
-export const postInventory = async ({ steamid, key }) => {
-  const res = await axios.post(`${BACKEND}/api/inventory`, { steamid, key }, { withCredentials: true });
-  return res.data;
-};
